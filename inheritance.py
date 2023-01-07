dd6442f5af98e38444c0852d75cd1c2b7a5d3c73
hsynhsyn77 <108232382+hsynhsyn77@users.noreply.github.com>
2023-01-08 02:38:23 +0300

    python13

--- inheritance.py
+++ inheritance.py
@@ -1,3 +1,4 @@
+"""
 class Employees():
     def __init__(self):
         self.FirstName = ""
@@ -31,4 +32,55 @@ class Employees_Yeni():
 ali = Employees_Yeni("g","b","c")
 print(ali.FirstName)
 print(ali.LastName)
-print(ali.Adress)
+print(ali.Adress) """
+
+
+# inheritance (kalıtım)
+# Person => name, lastname,  age, eat(), run(), drink
+# student (person), teacher (person)
+# Animal => Dog(Animal), Cat(Animal)
+class Person():
+    def __init__(self, fname, lname):
+        self.firstName = fname
+        self.lastName = lname
+        print("Person Created")
+
+    def who_am_i(self):
+        print("I am a person")
+
+    def eat(self):
+        print("I am a eating")
+
+
+class Student(Person):
+    def __init__(self, fname, lname, number):
+        Person.__init__(self, fname, lname)
+        self.studentNumber = number
+        print("Student Created")
+    # override
+    def who_am_i(self):
+        print("I am a student")
+    def sayHello(self):
+        print("Hello I am a student")
+class Teacher(Person):
+    def __init__(self, fname, lname, branch):
+        super().__init__(fname, lname)
+        self.branch = branch
+    def who_am_i(self):
+        print(f"I am a {self.branch} teacher")
+
+
+p1 = Person("kaan gökhan", "şirikçi")
+s1 = Student("simay elif", "şirikçi", 1234)
+t1 = Teacher("hüseyin", "şirikçi", "Python")
+
+t1.who_am_i()
+
+print(p1.firstName + " " + p1.lastName)
+print(s1.firstName + " " + s1.lastName + " " + str(s1.studentNumber))
+
+p1.who_am_i()
+s1.who_am_i()
+s1.eat()
+p1.eat()
+s1.sayHello()
