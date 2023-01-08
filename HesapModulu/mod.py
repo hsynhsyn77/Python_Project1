a323da1b3c789a4eb615503ee54b3e61f7f9d314
hsynhsyn77 <108232382+hsynhsyn77@users.noreply.github.com>
2023-01-08 18:22:33 +0300

    python18

---
+++ HesapModulu/mod.py
@@ -0,0 +1,23 @@
+"""
+modül hakkında bilgilendirme
+"""
+print("modül eklendi")
+number = 10
+
+numbers = [1, 2, 3]
+
+person = {
+    "name": "kaan",
+    "age": "25",
+    "city": "Konya"
+}
+def func(x):
+    '''
+    fonksiyon hakında biligilendirme
+    '''
+    print(f"x: {x}")
+
+class Person:
+    def speak(self):
+        print("I am speaking")
+
--- HesapModulu/test.py
+++ HesapModulu/test.py
@@ -1,18 +1,1 @@
-import hesapmodulu
 
-maas=hesapmodulu.yeni_maas(1000)
-print(maas)
-maas2=hesapmodulu.yeni_maas(2000)
-print(maas2)
-
-import hesapmodulu as hm
-hm1=hm.yeni_maas(1000)
-print(hm1)
-
-from hesapmodulu import yeni_maas
-maas2=yeni_maas(4000)
-print(maas2)
-
-import hesapmodulu as hm
-maaslar1=hm.maaslar
-print(maaslar1)
