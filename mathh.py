13362fd31c5f9fbdae8d3c1ef5cd60526a2bce8c
hsynhsyn77 <108232382+hsynhsyn77@users.noreply.github.com>
2023-01-08 14:11:18 +0300

    python16

---
+++ mathh.py
@@ -0,0 +1,25 @@
+# Yöntem 1
+
+# import math
+import mathh as islem
+# value = dir(math)
+# value=help(math)
+# value=help(math.factorial)
+# value=math.sqrt(49)
+# value = islem.factorial(5)
+
+# Yöntem 2
+from math import *
+
+
+def sqrt(x):
+    print("x : " + str(x))
+from math import factorial, sqrt, ceil
+
+# value=factorial(5)
+# value=math.sqrt(14)
+# value=factorial(5)
+# value=ceil(9.8)
+value = sqrt(9)
+
+print(value)
