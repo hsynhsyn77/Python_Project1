5f34daa234da97e8e3cdd2cf7649200fb1c76a9f
hsynhsyn77 <108232382+hsynhsyn77@users.noreply.github.com>
2023-01-08 23:24:40 +0300

    python22

--- asal-sayı.py
+++ asal-sayı.py
@@ -1,13 +1,14 @@
 sayi = int(input("sayı : "))
-asalmi=True
+asalmi = True
 
 if sayi == 1:
-   asalmi=False
+    asalmi = False
 for i in range(2, sayi):
     if (sayi % i == 0):
-        asalmi=False
+        asalmi = False
         break
 if asalmi:
     print("sayı asaldır")
 else:
     print("sayı asal değildir")
+
---
+++ files.py
@@ -0,0 +1,16 @@
+#file = open("newfile.txt", "w")
+#file.close()
+
+#file=open("/Users/huseyin/newfile.py","w")
+#print(file)
+
+#file = open("newfile.txt", "w",encoding="utf-8")
+#file.write("\nhüseyin şirikçi")
+#file.close()
+
+#file = open("newfile.txt", "a",encoding="utf-8")
+#file.write("\nkaan gökhan")
+#file.write("kaan gökhan\n")
+#file.close()
+
+file=open("newfile2.txt", "x",encoding="utf-8")
