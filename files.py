438d8e992333155f5fda80481da841cadcc0e46c
hsynhsyn77 <108232382+hsynhsyn77@users.noreply.github.com>
2023-01-09 20:29:31 +0300

    python24

--- files.py
+++ files.py
@@ -8,9 +8,9 @@
 #file.write("\nhüseyin şirikçi")
 #file.close()
 
-#file = open("newfile.txt", "a",encoding="utf-8")
-#file.write("\nkaan gökhan")
-#file.write("kaan gökhan\n")
+file = open("newfile.txt", "a",encoding="utf-8")
+file.write("\nkaan gökhan")
+file.write("kaan gökhan\n")
 #file.close()
 
 file=open("newfile2.txt", "x",encoding="utf-8")
---
+++ traversing.py
@@ -0,0 +1,7 @@
+with open("newfile.txt", "r", encoding="utf-8") as file:
+    content = file.read(10)
+    print(content)
+    file.seek(0) #""" index e gönderir """
+    print(file.tell())
+    content2 = file.read()
+    print(content2)
