4fda004b651156206c3fac9b9f0f02724fd6b63f
hsynhsyn77 <108232382+hsynhsyn77@users.noreply.github.com>
2023-01-20 18:31:59 +0300

    twitter.py1

--- Selenium/twitter.py
+++ Selenium/twitter.py
@@ -22,23 +22,34 @@ class Twitter:
         passwordInput = self.browser.find_element(By.XPATH, "//*[@name='password']")
         passwordInput.send_keys(self.password)
         passwordInput.send_keys(Keys.ENTER)
-        time.sleep(2)
-
-
-
-    def search(self, hashtag):
-        searchınput = self.browser.find_element(By.XPATH,"/html//div[@id='react-root']/div/div/div[2]/main[@role='main']/div/div/div/div[2]/div[@class='css-1dbjc4n r-1pi2tsx']/div[2]/div/div[@class='css-1dbjc4n']//form[@role='search']//label/div[@class='css-1dbjc4n r-16y2uox r-1wbh5a2']/div/input[@role='combobox']")
-        searchınput.send_keys(hashtag)
-        time.sleep(2)
-        searchınput.send_keys(Keys.ENTER)
         time.sleep(4)
 
-        list = self.browser.find_elements(By.XPATH, "//article[@data-testid='tweet']/div[1]/div[1]/div[1]/div[2]/div[2]/div[2]/div[1]")
+    def search(self, hashtag):
+        searchınput = self.browser.find_element(By.XPATH,
+                                                "/html//div[@id='react-root']/div/div/div[2]/main[@role='main']/div/div/div/div[2]/div[@class='css-1dbjc4n r-1pi2tsx']/div[2]/div/div[@class='css-1dbjc4n']//form[@role='search']//label/div[@class='css-1dbjc4n r-16y2uox r-1wbh5a2']/div/input[@role='combobox']")
+        searchınput.send_keys(hashtag)
+        time.sleep(3)
+        searchınput.send_keys(Keys.ENTER)
+        time.sleep(4)
+        loopCounter = 0
+        last_height = self.browser.execute_script("return document.documentElement.scrollHeight")
+        while True:
+            if loopCounter > 5:
+                break
+                self.browser.execute_script("window.scrollTo(0,documentElement.scrollHeight)")
+            time.sleep(2)
+            new_height = self.browser.execute_script("return documentElement.scrollHeight")
+            if last_height == new_height:
+                break
+            last_height = new_height
+            loopCounter+=1
+
+        list = self.browser.find_elements(By.XPATH,
+                                          "//article[@data-testid='tweet']/div[1]/div[1]/div[1]/div[2]/div[2]/div[2]/div[1]")
         for item in list:
-            print("*****************")
+            print("***************** ")
             print(item.text)
-
-
+        print(len(list))
 
 
 twitter = Twitter(username, password)
