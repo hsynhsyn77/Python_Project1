89e163f176bf2299c77765cb63206bef26ef47a7
hsynhsyn77 <108232382+hsynhsyn77@users.noreply.github.com>
2023-01-20 23:12:40 +0300

    twitter.py2

--- Selenium/twitter.py
+++ Selenium/twitter.py
@@ -29,21 +29,21 @@ class Twitter:
                                                 "/html//div[@id='react-root']/div/div/div[2]/main[@role='main']/div/div/div/div[2]/div[@class='css-1dbjc4n r-1pi2tsx']/div[2]/div/div[@class='css-1dbjc4n']//form[@role='search']//label/div[@class='css-1dbjc4n r-16y2uox r-1wbh5a2']/div/input[@role='combobox']")
         searchınput.send_keys(hashtag)
         time.sleep(3)
+
         searchınput.send_keys(Keys.ENTER)
-        time.sleep(4)
+        time.sleep(3)
         loopCounter = 0
         last_height = self.browser.execute_script("return document.documentElement.scrollHeight")
         while True:
             if loopCounter > 5:
                 break
-                self.browser.execute_script("window.scrollTo(0,documentElement.scrollHeight)")
+                self.browser.execute_script("window.scrollTo(0,document.documentElement.scrollHeight)")
             time.sleep(2)
-            new_height = self.browser.execute_script("return documentElement.scrollHeight")
+            new_height = self.browser.execute_script("return document.documentElement.scrollHeight")
             if last_height == new_height:
                 break
             last_height = new_height
             loopCounter+=1
-
         list = self.browser.find_elements(By.XPATH,
                                           "//article[@data-testid='tweet']/div[1]/div[1]/div[1]/div[2]/div[2]/div[2]/div[1]")
         for item in list:
