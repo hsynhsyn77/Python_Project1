7618bfcc03e09241b7ef97f5f0bf6e1346c19f7b
hsynhsyn77 <108232382+hsynhsyn77@users.noreply.github.com>
2023-03-11 14:24:26 +0300

    pytq56

---
+++ _radiobutton.py
@@ -0,0 +1,55 @@
+from PyQt5 import QtWidgets
+import sys
+from _radioButtonForm import Ui_MainWindow
+
+
+class Window(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.radioTurkiye.setChecked(True)
+        self.ui.radioLise.setChecked(True)
+
+        self.ui.radioTurkiye.toggled.connect(self.onClickedUlke)
+        self.ui.radioAzerbeycan.toggled.connect(self.onClickedUlke)
+        self.ui.radioAlmanya.toggled.connect(self.onClickedUlke)
+        self.ui.radioYunanistan.toggled.connect(self.onClickedUlke)
+
+        self.ui.radioilkokul.toggled.connect(self.onClickedEgitim)
+        self.ui.radioLise.toggled.connect(self.onClickedEgitim)
+        self.ui.radioniversite.toggled.connect(self.onClickedEgitim)
+        self.ui.radioYukseklisans.toggled.connect(self.onClickedEgitim)
+
+        self.ui.btnUlke.clicked.connect(self.getSelectedUlke)
+        self.ui.btnEgitim.clicked.connect(self.getSelectedEgitim)
+
+    def onClickedUlke(self):
+        rb = self.sender()
+        if rb.isChecked():
+            print("seçilen ülke:" + rb.text())
+
+    def onClickedEgitim(self):
+        rb = self.sender()
+        if rb.isChecked():
+            print("seçilen eğitim:" + rb.text())
+
+    def getSelectedUlke(self):
+        items = self.ui.groupBoxUlke.findChildren(QtWidgets.QRadioButton)
+        for rb in items:
+            if rb.isChecked():
+                self.ui.lblUlke.setText("seçilen ülke :"+ rb.text())
+
+    def getSelectedEgitim(self):
+        items = self.ui.groupBoxEgitim.findChildren(QtWidgets.QRadioButton)
+        for rb in items:
+            if rb.isChecked():
+                self.ui.lblEgitim.setText("seçilen Eğitim :"+ rb.text())
+
+
+app = QtWidgets.QApplication(sys.argv)
+win = Window()
+win.show()
+sys.exit(app.exec_())
